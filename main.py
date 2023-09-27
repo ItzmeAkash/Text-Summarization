116ff9136c56832c5aebb3885dcd509ed4ce4a7f
Akash P S <116643229+ItzmeAkash@users.noreply.github.com>
2023-09-27 13:50:06 +0530

    model evaluation completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionPipelin
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationPipeline
 from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerPipeline
+from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from textSummarizer.logging import logger
 
 
@@ -55,3 +56,16 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+STAGE_NAME =  "Model Evaluation Stages"
+
+
+try:
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} Started <<<<<<<<<<<<<")
+    model_evaluation = ModelEvaluationTrainingPipeline()
+    model_evaluation.main()
+    logger.info(f">>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx===========x")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
